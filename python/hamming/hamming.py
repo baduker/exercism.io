c9ebe451f3f9280ea35bc0a7791954e64e115035
baduker <baduker@protonmail.ch>
2019-07-18 10:16:52 +0200

    Adding new solutions

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,4 +2,4 @@ def distance(str_a, str_b):
     if len(str_a) != len(str_b):
         raise ValueError("Strand lengths are not equal!")
     else:
-        return sum(str_a != str_b for str_a, str_b in zip(str_a, str_b))
+        return sum(1 for (a, b) in zip(str_a, str_b) if a != b)
