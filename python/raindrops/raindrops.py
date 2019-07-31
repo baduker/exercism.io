1a383a15e41449db670465b8d01383acd9f1b13f
baduker <baduker@protonmail.ch>
2019-07-31 16:57:13 +0200

    New solution.

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,14 @@
+SOUNDS = {3: "Pling", 5: "Plang", 7: "Plong"}
+FACTORS = (3, 5, 7)
+
+def is_divisible_by(number, divisior):
+    return number % divisior == 0
+
+
+def sounds(number):
+    return [SOUNDS[factor] for factor in FACTORS if is_divisible_by(number, factor)]
+
+
+def convert(number):
+    speak = sounds(number)
+    return "".join(speak) if speak else str(number)
