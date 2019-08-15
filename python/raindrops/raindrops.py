04f5b403395a8fa9d48989fd5b95eada028c5cee
baduker <baduker@protonmail.ch>
2019-08-15 11:28:00 +0200

    Added a list of sets instead a dict.

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,14 +1,9 @@
-SOUNDS = {3: "Pling", 5: "Plang", 7: "Plong"}
-FACTORS = (3, 5, 7)
-
-def is_divisible_by(number, divisior):
-    return number % divisior == 0
+SOUNDS = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
 
 
-def raidndrops(number):
-    return [SOUNDS[factor] for factor in FACTORS if is_divisible_by(number, factor)]
+def raindrops(number):
+    return [sound for d, sound in SOUNDS if number % d == 0]
 
 
 def convert(number):
-    speak = raidndrops(number)
-    return "".join(speak) if speak else str(number)
+    return "".join(raindrops(number)) or str(number)
