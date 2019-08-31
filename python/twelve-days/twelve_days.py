34670b18aa2e7f351567a77573b167ab55a755e6
baduker <baduker@protonmail.ch>
2019-08-31 21:37:52 +0200

    Updated solution.

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 
 
-verses = {
-        1: ["first", "and a Partridge in a Pear Tree."],
-        2: ["second", "two Turtle Doves, "],
-        3: ["third", "three French Hens, "],
-        4: ["fourth", "four Calling Birds, "],
-        5: ["fifth", "five Gold Rings, "],
-        6: ["sixth", "six Geese-a-Laying, "],
-        7: ["seventh", "seven Swans-a-Swimming, "],
-        8: ["eighth", "eighcdt Maids-a-Milking, "],
-        9: ["ninth", "nine Ladies Dancing, "],
-        10: ["tenth", "ten Lords-a-Leaping, "],
-        11: ["eleventh", "eleven Pipers Piping, "],
-        12: ["twelfth", "twelve Drummers Drumming, "]
-        }
+verses = [["first", "and a Partridge in a Pear Tree."],
+          ["second", "two Turtle Doves, "],
+          ["third", "three French Hens, "],
+          ["fourth", "four Calling Birds, "],
+          ["fifth", "five Gold Rings, "],
+          ["sixth", "six Geese-a-Laying, "],
+          ["seventh", "seven Swans-a-Swimming, "],
+          ["eighth", "eight Maids-a-Milking, "],
+          ["ninth", "nine Ladies Dancing, "],
+          ["tenth", "ten Lords-a-Leaping, "],
+          ["eleventh", "eleven Pipers Piping, "],
+          ["twelfth", "twelve Drummers Drumming, "]]
 
 
 def get_verses(line):
-    verse = f"On the {verses[line][0]} day of Christmas " + \
-        "my true love gave to me: "
-    for i in range(line, 0, -1):
+    verse = f"On the {verses[line][0]} day of Christmas my true love gave to me: "
+    for i in range(line, -1, -1):
         verse += f"{verses[i][1]}"
-    return verse.replace("and ", "") if line == 1 else verse
+    return verse.replace("and ", "") if line == 0 else verse
 
 
 def recite(start_verse, end_verse):
-    return [get_verses(line) for line in range(start_verse, end_verse + 1)]
+    return [get_verses(line_n)
+            for line_n in range(start_verse - 1, end_verse)]
