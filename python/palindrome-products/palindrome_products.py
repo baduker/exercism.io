573a18ad4375761d652435360a8fbf499ff1e07d
baduker <baduker@protonmail.ch>
2019-10-13 18:59:22 +0200

    New challange

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,20 +1,34 @@
-def is_palindrome(min, max):
-    palindromes = {}
-    for i in range(min, max):
-        for j in range(i, max):
-            product = i * j
-            if str(product) == str(product)[::-1]:
-                palindromes[product] = {i, j}
-    return palindromes
+def palindromes(product_range):
+    return (int(x) for x in map(str, product_range) if x == x[::-1])
+
+
+def find_palindromes(min_factor, max_factor, *, reverse=False):
+    if max_factor < min_factor:
+        raise ValueError("Invalid input!")
+
+    number_ragne = range(min_factor, max_factor + 1)
+    product_range = range(min_factor ** 2, max_factor ** 2 + 1)
+
+    if reverse:
+        product_range = reversed(product_range)
+
+    for palindrome in palindromes(product_range):
+        factors = [
+            [item, palindrome // item]
+            for item in number_ragne
+            if palindrome % item == 0
+            and palindrome // item in number_ragne
+            and item <= palindrome // item
+        ]
+        if factors:
+            return (palindrome, factors)
+
+    return None, []
 
 
 def largest(min_factor, max_factor):
-    palindromes = is_palindrome(min_factor, max_factor + 1)
-    largest = max(palindromes)
-    return (largest, palindromes[largest])
+    return find_palindromes(min_factor, max_factor, reverse=True)
 
 
 def smallest(min_factor, max_factor):
-    palindromes = is_palindrome(min_factor, max_factor + 1)
-    smallest = min(palindromes)
-    return (smallest, palindromes[smallest])
+    return find_palindromes(min_factor, max_factor)
