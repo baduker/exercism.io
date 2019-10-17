2ad8d438f89faa0f6bd8983f644b11493f8c8298
baduker <baduker@protonmail.ch>
2019-10-17 09:00:19 +0200

    New solution

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,3 +1,19 @@
+import random
+
+
 class Character:
     def __init__(self):
-        pass
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        return sum(sorted(random.sample(range(1, 7), 4))[1:])
+
+
+def modifier(mod_value):
+    return (mod_value - 10) // 2
