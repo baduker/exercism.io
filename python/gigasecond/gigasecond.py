8f027acd24244929e1749ab66855682ffc8ce8de
baduker <baduker@protonmail.ch>
2019-10-24 11:54:55 +0200

    Adding new track - ruby

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -2,5 +2,5 @@ from datetime import datetime, timedelta
 
 
 def add(a_date):
-  GIGASECOND = timedelta(seconds = 10**9)
-  return a_date + GIGASECOND
+    GIGASECOND = timedelta(seconds=10**9)
+    return a_date + GIGASECOND
---
+++ ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,5 @@
+class Gigasecond
+    def self.from(time_of_birth)
+        time_of_birth + 10**9
+    end
+end
