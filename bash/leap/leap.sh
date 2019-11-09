1fd9d201b855a5eb88858923abe4a0652efcb837
baduker <baduker@protonmail.ch>
2019-11-09 17:37:34 +0100

    New track: bash; and new solutions

---
+++ bash/leap/leap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+main () {
+
+if [ $(($1 % 4)) -eq 0 ]  && [  $(($1 % 100)) -ne 0 ] || [ $(($1 % 400)) -eq 0 ]
+then
+    echo "true"
+else
+    echo "false"
+fi
+
+}
+
+main "$@"
