1b76ac7073e7ec0457ab7f5acef4419b43a45291
baduker <baduker@protonmail.ch>
2020-01-13 09:12:39 +0100

    New Go solution

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,7 +1,29 @@
+// Package letter implements letter frequency counter using
+// parallel computation.
 package letter
 
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
+
+// ConcurrentFrequency counts the frequency of letters
+func ConcurrentFrequency(source []string) FreqMap {
+	ch := make(chan FreqMap)
+
+	for _, text := range source {
+		go func(word string) {
+			ch <- Frequency(word)
+		}(text)
+	}
+
+	countMap := FreqMap{}
+
+	for range source {
+		for letter, count := range <-ch {
+			countMap[letter] += count
+		}
+	}
+	return countMap
+}
 
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
