9c29aa984d987103afdf6fafdd9c9eb7316b514c
baduker <baduker@protonmail.ch>
2020-07-12 10:22:25 +0200

    New language, new tasks

---
+++ rust/armstrong-numbers/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let my_str = num.to_string();
+    let power = my_str.len();
+    let mut sum = 0;
+
+    for ch in my_str.chars() {
+        sum += ch.to_string().parse::<u32>().unwrap().pow(power as u32);
+    }
+    sum == num
+}
